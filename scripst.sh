0ec6087f9e1a8b61327d94b537e399d3fe38e163
huijie liu <huijieliu@buaa.edu.cn>
2024-04-03 19:09:26 +0800

    Bug fix

---
+++ scripst.sh
@@ -0,0 +1,2 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple package
+bash scripts/finetune_real.sh "cat" data/cat real_reg/samples_cat  cat finetune_addtoken.yaml checkpoints/sd-v1-4.ckpt > train.log 2>&1
