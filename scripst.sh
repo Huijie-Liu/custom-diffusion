5d34f5f0cd597d762331b98dc8fd7e7579eb51af
huijie liu <1627158922@qq.com>
2024-04-07 22:29:25 +0800

    Bug fix

--- scripst.sh
+++ scripst.sh
@@ -1,2 +1,3 @@
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple package
 bash scripts/finetune_real.sh "cat" data/cat real_reg/samples_cat  cat finetune_addtoken.yaml checkpoints/sd-v1-4.ckpt > train.log 2>&1
+git clone https://githubfast.com/xxxxx
